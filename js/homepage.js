5ae96df96f3d985f2b6ae746b2ea3cf00d1d9b0c
THE UNDERTAKER!!@! <matanso@gmail.com>
2018-01-31 16:47:52 +0200

    Simple reorganization

--- js/homepage.js
+++ js/homepage.js
@@ -1,62 +1,57 @@
 $(document)
-  .ready(function() {
+    .ready(function () {
 
-    var
-      changeSides = function() {
-        $('.ui.shape')
-          .eq(0)
-            .shape('flip over')
-            .end()
-          .eq(1)
-            .shape('flip over')
-            .end()
-          .eq(2)
-            .shape('flip back')
-            .end()
-          .eq(3)
-            .shape('flip back')
-            .end()
-        ;
-      },
-      validationRules = {
-        firstName: {
-          identifier  : 'name',
-          rules: [
-            {
-              type   : 'empty',
-              prompt : 'Please enter your name'
-            }
-          ]
-        },
-        email: {
-          identifier  : 'email',
-          rules: [
-            {
-              type   : 'email',
-              prompt : 'Please enter a valid e-mail'
-            }
-          ]
-        }
-      }
-    ;
+        var
+            changeSides = function () {
+                $('.ui.shape')
+                    .eq(0)
+                    .shape('flip over')
+                    .end()
+                    .eq(1)
+                    .shape('flip over')
+                    .end()
+                    .eq(2)
+                    .shape('flip back')
+                    .end()
+                    .eq(3)
+                    .shape('flip back')
+                    .end()
+                ;
+            },
+            validationRules = {
+                firstName: {
+                    identifier: 'name',
+                    rules: [
+                        {
+                            type: 'empty',
+                            prompt: 'Please enter your name'
+                        }
+                    ]
+                },
+                email: {
+                    identifier: 'email',
+                    rules: [
+                        {
+                            type: 'email',
+                            prompt: 'Please enter a valid e-mail'
+                        }
+                    ]
+                }
+            };
 
-    $('.ui.dropdown')
-      .dropdown({
-        on: 'hover'
-      })
-    ;
+        $('.ui.dropdown')
+            .dropdown({
+                on: 'hover'
+            });
 
-    $('.ui.form')
-      .form(validationRules, {
-        on: 'blur'
-      })
-    ;
+        $('.ui.form')
+            .form(validationRules, {
+                on: 'blur'
+            });
 
-    $('.masthead .information')
-      .transition('scale in', 1000)
-    ;
+        $('.masthead .information')
+            .transition('scale in', 1000);
 
-    setInterval(changeSides, 3000);
+        setInterval(changeSides, 3000);
 
-  })
-;
+    });
